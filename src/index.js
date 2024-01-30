0b360da4c0f3013302de342f2900b979c6fbd45a
Juan Guzman <juanltd07@gmail.com>
2024-01-30 08:22:52 -0600

    feat: react dom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import 'react-app-polyfill/stable'
 import 'core-js'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store, persistor } from './store'
 import { PersistGate } from 'redux-persist/integration/react'
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 )
