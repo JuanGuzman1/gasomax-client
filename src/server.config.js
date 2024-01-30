eb676bab0f976dde9daa0029154f34396a7b5bed
Juan Guzman <juanltd07@gmail.com>
2024-01-30 15:33:58 -0600

    server config

--- src/server.config.js
+++ src/server.config.js
@@ -3,7 +3,7 @@ const config = {
     baseURL:
       process.env.NODE_ENV === 'development'
         ? 'http://localhost:8000'
-        : 'https:/cms.grupoguerra.com',
+        : 'https://api-test.grupoguerramotors.com/',
     timeout: 20000,
     headers: {
       'Content-Type': 'application/json',
